349a55aa5e325cdfd0309aaadabb4cc742ab3698
Karen Moreno <kmorenom@credicorpcapital.com>
2024-03-24 20:00:21 -0500

    Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '/Users/kmorenom/Developer/red_social_inla_ze/src/Document/Swagger.json';
 
 import connectDB from './database';
 import bodyParser from 'body-parser';
@@ -15,6 +17,8 @@ const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
+// Configurar Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/auth', AuthRouter);
 app.use('/user', UserRoute);
 app.use('/post', PostRoute);
